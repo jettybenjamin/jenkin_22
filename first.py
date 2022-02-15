3d93d1ed8df9d489603bddde9feeb6c8dcd57a31
jettybenjamin <jettybenjamin@amaljyothi.ac.in>
2022-02-15 12:37:41 +0530

    second commit

--- first.py
+++ first.py
@@ -1,1 +1,14 @@
 print("hello")
+n=int(input("enter the limit"))
+a = 0
+b = 1
+if n == 1:
+    print(a)
+else:
+    print(a)
+    print(b)
+    for i in range(2,n+1):
+        c = a + b
+        a = b
+        b = c
+        print(c)
